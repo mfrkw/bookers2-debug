3d3933b22f13a4a00ce66a18d7e055d041a354c4
mfrkw <xjdk3766@gmail.com>
2021-07-02 13:10:04 +0000

    [いいね]

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
+  def already_favorited?(book)
+    self.favorites.exists?(book_id: book.id)
+  end 
+  
+    
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :books
-  resources :users,only: [:show,:index,:edit,:update]
+  
+  resources :users, only: [:show,:index,:edit,:update]
 
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end 
 end 
