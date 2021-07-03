8f03f84bbee76f62284a92e44a19424c6185a336
mfrkw <xjdk3766@gmail.com>
2021-07-03 14:40:45 +0000

    [コメント機能]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -12,7 +12,8 @@ class BookCommentsController < ApplicationController
   
   def destroy
     @book = Book.find(params[:book_id])
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    book_comment = current_user.book_comments.find_by(book_id: @book_id)
+    book_comment.destroy
     redirect_back(fallback_location: root_path)
   end
   
