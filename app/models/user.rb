17eed1f9d807fd83dacf318f9ab854dd2532338c
mfrkw <xjdk3766@gmail.com>
2021-07-04 07:01:27 +0000

    [フォロー機能 途中]

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :relationships,
 
   def already_favorited?(book)
     self.favorites.exists?(book_id: book.id)
