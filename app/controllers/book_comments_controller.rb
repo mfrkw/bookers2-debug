e2d480241426896ef36dcc1bc07f653dd62ae826
mfrkw <xjdk3766@gmail.com>
2021-07-06 07:23:41 +0000

    [コメント機能修正]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,11 +2,14 @@ class BookCommentsController < ApplicationController
   
   def create
     @book = Book.find(params[:book_id])
-    book_comment = current_user.book_comments.new(book_comment_params)
-    book_comment.book_id = @book.id
-    book_comment.save!
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    if @book_comment.save
     redirect_back(fallback_location: root_path)
-  
+    else
+      @user = @book.user
+      render 'books/show'
+    end 
   end
   
   
--- app/models/book_comment.rb
+++ app/models/book_comment.rb
@@ -3,5 +3,6 @@ class BookComment < ApplicationRecord
   belongs_to :book
   
   validates :comment, presence: true
+  
 	
 end
